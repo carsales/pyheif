6bd22d5ce17bdad28b2f396fa9a215eb06fc9203
David Poirier <david-poirier-csn@users.noreply.github.com>
2019-04-26 16:36:50 +1000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pyheif',
-    version='0.3',
+    version='0.3.1',
     packages=find_packages(),
     install_requires=['cffi>=1.0.0'],
     setup_requires=['cffi>=1.0.0'],
