e90c46120a904f176b802ecd47344c1f7547b434
David Poirier <david-poirier-csn@users.noreply.github.com>
2020-07-11 21:23:29 +1000

    fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -f docker/Dockerfile.manylinux2014 --build-arg PYPI_USERNAME --build-arg PYPI_PASSWORD .
+echo "Pypi password for $PYPI_USERNAME: "
+read -s password
+
+docker build -f docker/Dockerfile.manylinux2014 --build-arg PYPI_USERNAME --build-arg PYPI_PASSWORD=$password .
+
