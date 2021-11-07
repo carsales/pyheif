247465b016a207126752838689a234e4ebfdc1b6
Alexander Karpinsky <homm86@gmail.com>
2021-11-07 19:13:18 +0300

    mv docker/Dockerfile.manylinux2014 Dockerfile

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 echo "Pypi password for $PYPI_USERNAME: "
 read -s password
 
-docker build -f docker/Dockerfile.manylinux2014 --build-arg PYPI_USERNAME --build-arg PYPI_PASSWORD=$password .
+docker build --build-arg PYPI_USERNAME --build-arg PYPI_PASSWORD=$password .
 
